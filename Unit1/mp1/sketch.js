6de307b5fe87e5d1baea111d6db687fb31b841a1
maalikjhopkins <maalikjhopkins@gmail.com>
2022-01-25 09:30:38 -0600

    yes

---
+++ Unit1/mp1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+}
+
+function draw() {
+
+  if (mouseIsPressed) {
+    background('#2D936C');
+  } else {
+    background("yellow");
+  }
+
+  rect(width / 2, height / 2, 200, 100);
+
+}
