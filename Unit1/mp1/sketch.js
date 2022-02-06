72605d60a064488de562547b79cd2d721520c9ae
maalikjhopkins <maalikjhopkins@gmail.com>
2022-02-06 16:23:46 -0600

    yes

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,6 +1,7 @@
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
+  textAlign(LEFT);
 }
 
 function draw() {
@@ -13,4 +14,9 @@ function draw() {
 
   rect(width / 2, height / 2, 200, 100);
 
+  fill('black');
+  push();
+  rectMode(CORNER);
+  text("Lucas Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 10, 10, 400, 400);
+  pop();
 }
---
+++ Unit1/sounds/sketch.js
@@ -0,0 +1,18 @@
+let song1;
+
+function preload() {
+  song1 = loadSound("assets/GreenHillZone.mp3");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  song1 = play();
+}
+
+function draw() {
+
+}
+
+function touchStarted() {
+  getAudioContext().resume();
+}
