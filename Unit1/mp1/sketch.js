057ddc5ba78dafcddb36f3f39012af6f238b1d32
maalikjhopkins <maalikjhopkins@gmail.com>
2022-02-06 20:23:59 -0600

    yes

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,22 +1,74 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 800);
   rectMode(CENTER);
-  textAlign(LEFT);
+  ellipseMode(CENTER);
+  noStroke();
 }
 
 function draw() {
+  background(100);
 
   if (mouseIsPressed) {
-    background('#2D936C');
+    fill('black')
+    text("This is Inanna and she is a librarian who can cheat at life. She owns a book with a magic gem imbedded in the cover that allows her to come back to life anytime she should die, as if she was reloading a save file after getting a game over in a video game.", 500, 100, 900, 100);
+
+    // book
+    fill("brown");
+    rect(575, 250, 100, 150);
+
+    // magic gem
+    fill("yellow");
+    quad(590, 230, 560, 230, 550, 250, 600, 250);
+    quad(590, 270, 560, 270, 550, 250, 600, 250);
+
+    // body
+    fill("purple");
+    ellipse(350, 160, 100, 100);
+    quad(375, 200, 325, 200, 275, 400, 425, 400);
+    rect(315, 450, 25, 100);
+    rect(375, 450, 25, 100);
+    ellipse(303, 500, 50, 25);
+    ellipse(387, 500, 50, 25);
+    rect(450, 250, 145, 25);
+    ellipse(525, 250, 25, 25);
+
+    // facial expression
+    fill("black");
+    arc(350, 175, 50, 50, TWO_PI, PI);
+    ellipse(330, 145, 25, 25);
+    ellipse(370, 145, 25, 25);
+
+    fill("white");
+    ellipse(330, 145, 10, 10);
+    ellipse(370, 145, 10, 10);
   } else {
-    background("yellow");
+    fill('black')
+    text("This is Inanna and she is a librarian who can cheat at life. She owns a book with a magic gem imbedded in the cover that allows her to come back to life anytime she should die, as if she was reloading a save file after getting a game over in a video game.", 500, 100, 900, 100);
+
+    // body
+    fill("purple");
+    ellipse(350, 160, 100, 100);
+    quad(375, 200, 325, 200, 275, 400, 425, 400);
+    rect(315, 450, 25, 100);
+    rect(375, 450, 25, 100);
+    ellipse(303, 500, 50, 25);
+    ellipse(387, 500, 50, 25);
+
+    // facial expression
+    fill("black");
+    ellipse(350, 185, 50, 25);
+    ellipse(330, 145, 25, 25);
+    ellipse(370, 145, 25, 25);
+
+    fill("white");
+    ellipse(330, 137, 10, 10);
+    ellipse(370, 137, 10, 10);
+
+    // knife
+    fill("red");
+    triangle(350, 255, 330, 245, 330, 265);
+    rect(255, 255, 150, 20);
+    rect(230, 255, 10, 100);
   }
 
-  rect(width / 2, height / 2, 200, 100);
-
-  fill('black');
-  push();
-  rectMode(CORNER);
-  text("Lucas Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 10, 10, 400, 400);
-  pop();
 }
