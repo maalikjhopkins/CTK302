f43a6aab36c61ee1f4d99ccf04c65281ab387577
maalikjhopkins <maalikjhopkins@gmail.com>
2022-04-03 21:48:12 -0500

    h

--- Unit3/part1/sketch.js
+++ Unit3/part1/sketch.js
@@ -3,34 +3,52 @@ let cars = [];
 function setup() {
   createCanvas(500, 500);
 
-  for (let i = 0; i < 20; i++) {
-    cars.push(new Car());
+  // for (let i = 0; i < 20; i++) {
+  //   cars.push(new Car());
+  noStroke()
 }
-}
+
+
 
 function draw() {
   background("gray");
+  cars.push(new Car());
+
   for (let i = 0; i < cars.length; i++) {
   cars[i].display();
   cars[i].move();
+  if (cars[i].a <= 0) {
+    cars.splice(i, 1);
+  }
 }
 }
 
 class Car {
   constructor() {
     // attributes
-    this.pos = createVector(width/2, height/2);
-    this.vel = createVector(random(-3, 3), random(-3. 3));
+    this.pos = createVector(100, 100);
+    this.vel = createVector(random(10), random(10));
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
+    this.a = random(200, 255);
+    this.s = random(5, 20);
 
   }
 
   // methods
 
   display() {
-    ellipse(this.pos.x, this.pos.y, 20);
+    fill(this.r, this.g, this.b, this.a);
+    text("BABABOOEY", this.pos.x, this.pos.y, this.s);
   }
 
   move() {
     this.pos.add(this.vel);
+    this.a = this.a - 5;
+    // if (this.pos.x > width) this.pos.x = 0;
+    // if (this.pos.x < 0) this.pos.x = width;
+    // if (this.pos.y > height) this.pos.y = 0;
+    // if (this.pos.y < 0) this.pos.y = height;
   }
 }
